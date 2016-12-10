8f4462aae2b2fdf1723ebf396d23ff846e55ce9f
Julien Wilson <julienawilson@gmail.com>
2016-12-09 23:30:10 -0800

    setup.py formatting

--- setup.py
+++ setup.py
@@ -10,7 +10,10 @@ setup(
     author_email="julienawilson@gmail.com",
     license="MIT",
     package_dir={'': 'src'},
-    py_modules=["last", "string_to_array", "fake_binary", "count_positives_sum_negatives", "digitize", "binary_list_to_number", "sea_sick", "reverse_and_mirror", "flatten_me", "sum_from_string", "multiples"],
+    py_modules=["last", "string_to_array", "fake_binary",
+                "count_positives_sum_negatives", "digitize",
+                "binary_list_to_number", "sea_sick", "reverse_and_mirror",
+                "flatten_me", "sum_from_string", "multiples"],
     install_requires=[],
     extras_require={
         "test": ["pytest", "pytest-watch", "pytest-cov", "tox"]
