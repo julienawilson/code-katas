bb48382a3c8bc7fa4da30983b980019bc96c5ef5
Julien Wilson <julienawilson@gmail.com>
2016-12-09 23:19:50 -0800

    removed more files

--- .tox/py27/lib/python2.7/site-packages/binary_list_to_number.py
+++
@@ -1,9 +0,0 @@
-"""A function to convert list of ones and zeros to that binary zalue's equivalent integer."""
-
-
-def binary_list_to_number(arr):
-    """Convert list of ones and zeros to that binary zalue's equivalent integer."""
-    numb = 0
-    for i in range(len(arr)):
-        numb += arr[-(i + 1)] * 2**(i)
-    return numb
--- .tox/py27/lib/python2.7/site-packages/string_to_array.py
+++
@@ -1,8 +0,0 @@
-"""A function to convert a string to an array of words."""
-
-
-def string_to_array(string):
-    """Convert a string to array, even if empty."""
-    if string == "":
-        return [""]
-    return string.split()
