82dac6876e35d77be7ce9d110be13d43932d6877
Julien Wilson <julienawilson@gmail.com>
2016-12-11 09:34:12 -0800

    linter fix

--- src/series_sum.py
+++ src/series_sum.py
@@ -5,5 +5,5 @@ def series_sum(n):
     """Find the sum of the first nth terms of a series 1+1/4+1/7+..."""
     sum = 0
     for i in range(n):
-        sum += float(1/(1+i*3))
+        sum += float(1 / (1 + i * 3))
     return str("%.2f" % sum)
