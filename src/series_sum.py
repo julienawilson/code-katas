f58ff3b10fad9c90547712da00ac26a55518d7a5
Julien Wilson <julienawilson@gmail.com>
2016-12-11 09:27:59 -0800

    series_sum and test

---
+++ src/series_sum.py
@@ -0,0 +1,9 @@
+"""A function to find the sum of the first nth terms of a series 1+1/4+1/7+..."""
+
+
+def series_sum(n):
+    """Find the sum of the first nth terms of a series 1+1/4+1/7+..."""
+    sum = 0
+    for i in range(n):
+        sum += float(1/(1+i*3))
+    return str("%.2f" % sum)
---
+++ test_series_sum.py
@@ -0,0 +1,16 @@
+"""Tests for series_sum.series_sum."""
+import pytest
+
+
+ASSERTIONS = [
+    [1, "1.00"],
+    [2, "1.25"],
+    [3, "1.39"]
+]
+
+
+@pytest.mark.parametrize("n, result", ASSERTIONS)
+def test_series_sum(n, result):
+    """Test series_sum() for proper output in test cases."""
+    from series_sum import series_sum
+    assert series_sum(n) == result
