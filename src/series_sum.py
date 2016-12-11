24ce54c0c272270929ca5010a16ecc549a66bdfd
Julien Wilson <julienawilson@gmail.com>
2016-12-11 11:24:27 -0800

    fixed series_sum to work in py2 and removed redundant code

--- src/series_sum.py
+++ src/series_sum.py
@@ -3,7 +3,8 @@
 
 def series_sum(n):
     """Find the sum of the first nth terms of a series 1+1/4+1/7+..."""
-    sum = 0
+    total = 0
     for i in range(n):
-        sum += float(1 / (1 + i * 3))
-    return str("%.2f" % sum)
+        total += 1.0 / (1 + i * 3)
+    return "%.2f" % total
+ 
